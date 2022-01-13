a49e35b0a09c2f110a7cf5f333eb253206e0b068
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2022-01-13 15:38:47 +0200

    Gata

--- Recursivitate1/Program.cs
+++ Recursivitate1/Program.cs
@@ -6,7 +6,9 @@ namespace Recursivitate1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int[] vector = new int[] {5, 6, 7, 8, 9, 10};
+            Recursiv.ParcurgereVector(vector, vector.Length);
 
         }
     }
